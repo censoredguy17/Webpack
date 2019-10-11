36fa4b790f16291ddb1403b812a5f18209f2d707
Alexander <censoredguy17@gmail.com>
2019-10-11 15:05:06 +0300

    Webpack adding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function myModule(){
+
+    this.hello = function(){
+        return 'Hello';
+    }
+
+    this.goodBuy = function(){
+        return 'goodBuy';
+    }
+
+}
+
+module.exports = myModule;
